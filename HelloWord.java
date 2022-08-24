5b863f912fa81d804da3f4faf63d97a7f27c12f3
sandeepchitalkar <111955719+sandeepchitalkar@users.noreply.github.com>
2022-08-24 12:09:25 +0530

    Add files via upload

---
+++ HelloWord.java
@@ -0,0 +1,17 @@
+ class HelloWord {
+    public static void main(String[] args) {
+        //Following line is System.out.println() is used to print an argument that is passed to it on console as it is.
+        System.out.print(" Welcome sandeep !! in Java Programming !!\n");
+
+        /* The statement broken into 3 parts i.e. System, out and print or println
+         System: It is a final class defined in the java.lang package.
+         out: This is an instance of PrintStream type, which is a public and static member field of the System class.
+         println(): As all instances of PrintStream class have a public method println(),
+         hence we can invoke the same on out as well. This is an upgraded version of print().
+         It prints any argument passed to it and adds a new line to the output.
+         We can assume that System.out represents the Standard Output Stream.
+        */
+        System.out.println("Java Programming by Sandeep Chitalkar !!");
+
+    }
+}
